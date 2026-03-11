df284a4f02aa569331e950a5ee2381cd2476b33e
sarthakpersonal755-6581 <sarthakpersonal755@gmail.com>
2026-03-11 23:02:50 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,22 +9,8 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: 'SarthakDev | Web Developer',
   description: 'Web Developer building modern websites using Next.js and Flutter',
-  generator: 'v0.app',
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
+    icon: '/favicon.ico',
     apple: '/apple-icon.png',
   },
 }
